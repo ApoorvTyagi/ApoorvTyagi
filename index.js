cdd3f2a30b450c4879c4f85f50b682252a2bbef8
Apoorv Tyagi <39600765+ApoorvTyagi@users.noreply.github.com>
2020-08-22 22:38:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-rconst thisYear = new Date().getFullYear()
+const thisYear = new Date().getFullYear()
 const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
 const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
 const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
