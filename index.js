586589e056ec6eb2a96daf181e50e2af01c9b95b
Apoorv Tyagi <39600765+ApoorvTyagi@users.noreply.github.com>
2020-09-20 17:44:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const readme = `\
 
 ![](https://camo.githubusercontent.com/992babdffd8c74a1502de375fbdf7e4d54773242/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f53576f536b4e36447854737a71494b4571762f67697068792e676966)
 
-### About Me:
+### 🤵About Me:
 - 🏦 I'm currently working for a healthcare based startup where i make financial applications using spring boot and mysql 
       <img src="https://media.giphy.com/media/WUlplcMpOCEmTGBtBW/giphy.gif" width="30">
 - 🤔 I use daily \`\`\`.java\`\`\`,\`\`\` .py\`\`\`, \`\`\`.sql\`\`\`
@@ -55,7 +55,7 @@ const readme = `\
 </p>
 
 ---
-### My Github stats:
+### 🐱My Github stats:
 ![Apoorv's github stats](https://github-readme-stats.vercel.app/api?username=apoorvtyagi&show_icons=true&title_color=ffc857&icon_color=8ac926&text_color=daf7dc&bg_color=151515&hide=["stars"])
 [![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=apoorvtyagi&layout=compact&text_color=daf7dc&bg_color=151515)](https://github.com/anuraghazra/github-readme-stats)
 
@@ -75,7 +75,7 @@ const readme = `\
 <!-- BLOG-POST-LIST:END -->
 ---
 
-### Here's a Joke for you:
+### 😜Here's a Joke for you:
 <img src="https://readme-jokes.vercel.app/api" alt="Jokes Card" />
 `
 console.log(readme)
