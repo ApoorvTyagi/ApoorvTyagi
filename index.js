ab67bd7f0ed910168ef222ab76f5e4fdd17ab14c
Apoorv Tyagi <39600765+ApoorvTyagi@users.noreply.github.com>
2020-11-02 09:26:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const readme = `\
 - 🤔 I use daily \`\`\`.java\`\`\`,\`\`\` .py\`\`\`, \`\`\`.sql\`\`\`
 - 🌱 I’m currently learning docker and kubernetes
 - 💬 Talk to me about cricket, python (both of these are not any kind of insect or animals) and silicon valley
-- 😄 Pronouns: He/His/Him
+- 😄 Pronouns: He/Him
 - 📝 I regulary write articles on [hashnode](https://apoorvtyagi.tech/) (previously on [blogger](https://apoorvtyagi133.blogspot.com/))
 - 👯 We can connect on [Chess.com](https://www.chess.com/member/sweetxcyanide)♟ to play some games
 - ⚡ Fun fact: Je connais un peu le français
