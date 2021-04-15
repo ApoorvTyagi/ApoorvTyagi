ce391395cef09d70cf6e324968e22e99a23a3108
Apoorv Tyagi <39600765+ApoorvTyagi@users.noreply.github.com>
2021-04-15 09:34:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ const readme = `\
 
 ---
 
-### 🔉 An Interesting Fact/Quote:
+### 🔉 A Famous Fact/Quote:
 <a href="https://github.com/marketplace/actions/quote-readme">
 <!--STARTS_HERE_QUOTE_README-->
 <i>❝IMDb is one of the oldest websites on the internet, and began on Usenet in 1990 as a list of “actresses with beautiful eyes.”❞</i>
