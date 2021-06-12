5bcf5df28432f630d712b75df3cfea2b403467ca
Apoorv Tyagi <39600765+ApoorvTyagi@users.noreply.github.com>
2021-06-12 11:16:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -86,6 +86,6 @@ const readme = `\
 ---
 
 ### <img align ='center' src='https://media2.giphy.com/media/UQDSBzfyiBKvgFcSTw/giphy.gif?cid=ecf05e47p3cd513axbek3f56ti3jzizq8hincw20jauyyfyw&rid=giphy.gif' width ='29px'> Here's some humor for you:
-<img src="https://readme-jokes.vercel.app/api" alt="Refresh again to view Jokes Card" />
+<img src="https://readme-jokes.vercel.app/api" alt="Error fetching resource, Refresh again to view Jokes Card" />
 `
 console.log(readme)
