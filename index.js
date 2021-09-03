4ab77c3b99ce660b9ee0a4f65cbd1231aa85fffe
Apoorv Tyagi <39600765+ApoorvTyagi@users.noreply.github.com>
2021-09-03 22:56:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
 const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
 const progressBarOfThisYear = generateProgressBar()
 
+let monthNames = ["Jan","Feb","Mar","Apr", "May","Jun","Jul","Aug", "Sep", "Oct","Nov","Dec"];
+
 function generateProgressBar() {
     const progressBarCapacity = 30
     const passedProgressBarIndex = parseInt(progressOfThisYear * progressBarCapacity)
@@ -59,7 +61,7 @@ const readme = `\
 
 <!--END_SECTION:waka-->
 
-⏳ **Year Progress** ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} % as on ⏰ ${(new Date().getDate()-1)+'-'+ (new Date().getMonth()+1)+'-'+new Date().getFullYear()+'.'}
+⏳ **Year Progress** ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} % as on ⏰ ${(new Date().getDate()-1)+'-'+ monthNames[(new Date().getMonth()+1)]+'-'+new Date().getFullYear()+'.'}
 
 ---
 
